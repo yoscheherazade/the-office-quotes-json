67df17d4ae9b8046320e979f1abaaea3b526c31c
LaQuita <lgollman@wayne.edu>
2018-03-25 14:03:15 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+let btn = document.getElementById('btn').addEventListener('click', () => {
+    getQuote();
+});
+
+function getQuote() {
+    const xhr = new XMLHttpRequest();
+    const url = 'quotes.json';
+
+    xhr.open("GET", url, true);
+
+    xhr.onload = function () {
+        if (this.status === 200) {
+            let quotes = JSON.parse(this.responseText);
+
+            for (i in quotes) {
+                outputQuote = quotes[i].quote;
+                outputCharacter = quotes[i].character;
+
+                queryNum = 20;
+                randomNum = Math.floor((Math.random() * queryNum) + 1);
+
+                document.getElementById('output').innerHTML =
+                    quotes[randomNum].quote + " - " + quotes[randomNum].character;
+            }
+        }
+    }
+
+    xhr.send();
+}
